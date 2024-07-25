d78e5501c64966d983d1a8fb29a8383eb75f8ec3
Sebastien Roy <seroy@c2r2p9.42quebec.com>
2024-07-25 11:34:49 -0400

    configured dockerfiles for mariadb and wordpress/php

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -5,12 +5,12 @@ mysqld_safe --datadir=/var/lib/mysql &
 
 sleep 5
 
-service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" && \
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" && \
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" && \
-mysql -e "FLUSH PRIVILEGES" && \
+# service mysql start
+mysqld -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysqld -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" && \
+mysqld -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" && \
+mysqld -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" && \
+mysqld -e "FLUSH PRIVILEGES" && \
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown && \
 exec mysqld_safe
 
---
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sleep 10
+if !file_exists("/var/www/wordpress/wp-config.php")
+	wp config create ==allow-root \
+		--dbname=$SQL_DATABASE \
+		--dbuser=$SQL_USER \
+		--dbpass=$SQL_PASSWORD \
+		--dbhost=mariadb:3306 --path='/var/www/wordpress'
+wp core install
+wp user create
